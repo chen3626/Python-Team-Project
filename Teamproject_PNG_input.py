fbd8f21e8d9ef7eed35f1864fe0fad83592b5b55
chen3626 <71737027+chen3626@users.noreply.github.com>
2020-10-07 15:39:31 -0400

    Update Teamproject_PNG_input.py

--- Teamproject_PNG_input.py
+++ Teamproject_PNG_input.py
@@ -1,1 +1,27 @@
 """Taking PNG input"""
+#make sure all the py files are in the same folder or destination in your computer
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+'''function that asks,takes,converts user inputted PNG image'''
+
+def PNG_Input():
+    print('Enter the destination of the PNG:')
+    png_destination = input()
+    
+    img = mpimg.imread(png_destination)
+    '''
+    img is a matrix
+    each 3x3 set in img is a pixel
+    Each row in every 3x3 inner list correlates with RGB
+    #not sure what the columns represent yet
+    '''
+   
+    plt.imshow(img)
+    '''
+    plt.imshow() prints the image into 'plots'
+    '''
+    
+    return img
+
+
