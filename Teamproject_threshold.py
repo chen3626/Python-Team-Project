48e39c05ef62d81f23f3baea8157112b5d45b3f6
chen3626 <71737027+chen3626@users.noreply.github.com>
2020-10-15 12:54:45 -0400

    Update Teamproject_threshold.py

--- Teamproject_threshold.py
+++ Teamproject_threshold.py
@@ -1,1 +1,16 @@
 """Threshold function"""
+import numpy as np
+
+def Threshold(image):
+    result = np.empty(image.shape)
+    average = np.average(image)
+    for i in range(len(image)):
+        for j in range(len(image[0])):
+            if image[i][j] >= average:
+                result[i][j] += 0
+            else:
+                result[i][j] += 255
+    return result
+    
+    
+    
