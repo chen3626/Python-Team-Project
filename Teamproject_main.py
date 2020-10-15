f26ebca134ee4bbde7d53155f340d83c19cf1e36
chen3626 <71737027+chen3626@users.noreply.github.com>
2020-10-15 12:54:02 -0400

    Update Teamproject_main.py

--- Teamproject_main.py
+++ Teamproject_main.py
@@ -2,16 +2,13 @@
 #C:\Users\skcst\Desktop\Python_Team\PNG\Purdue_Arch.png
 import Teamproject_PNG_input as png
 import Teamproject_grayscale as gray
-
-import Teamproject_edgeblurnoise as blur
+import Teamproject_edgeblurnoise as noise
 import Teamproject_edgehighpassfiltering as edge
-
-
+import Teamproject_threshold as threshold
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
 
 #Acquire PNG input from user
 image_matrix = png.PNG_Input()
@@ -26,32 +23,20 @@ b ----> specific line from array
 '''convert to grayscale'''
 gray_image = gray.Grayscale(image_matrix)
 
+'''Edge blur/noise smoothing'''
+blur_image = noise.blur(gray_image)
 
-blur_image = blur.blur(gray_image)
-
+'''Edge Enhancement'''
 sobel_all = edge.sobel_all(blur_image)
 
-
-#print(image_matrix)
-#print(gray_image)
-=======
-
-
-
-
-
-
-
+'''Thresholding'''
+thresh_image = threshold.Threshold(sobel_all)
 
 
 '''plotting all the images'''
-f, axarr = plt.subplots(2,2)
+f, axarr = plt.subplots(2,3)
 axarr[0,0].imshow(image_matrix)
-
-axarr[0,1].imshow(blur_image, cmap="gray")
-axarr[1,0].imshow(sobel_all, cmap="gray")
-#axarr[1,1].imshow(sobel_y_image, cmap="gray")
-=======
-axarr[0,1].imshow(gray_image)
-#axarr[1,0].imshow()
-#axarr[1,1].imshow()
+axarr[0,1].imshow(gray_image, cmap='gray')
+axarr[0,2].imshow(blur_image, cmap='gray')
+axarr[1,0].imshow(sobel_all, cmap='gray')
+axarr[1,1].imshow(thresh_image, cmap='gray')
