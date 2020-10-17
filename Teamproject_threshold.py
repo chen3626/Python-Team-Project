f6770d0de6a31faec89d15f72d4cc84e000845d7
chen3626 <71737027+chen3626@users.noreply.github.com>
2020-10-17 13:43:42 -0400

    Update Teamproject_threshold.py

--- Teamproject_threshold.py
+++ Teamproject_threshold.py
@@ -1,9 +1,28 @@
-"""Threshold function"""
+
+'''
+===============================================================================
+ENGR 133 Fa 2020
+
+Assignment Information
+	Assignment:     Python Team Project (threshold)
+	Author(s):      Steve Chen, chen3626@purdue.edu
+                    Eric Mesina, emesina@purdue.edu
+                    Danny Mcnulty, mcnulty1@purdue.edu
+                    Kyle Nematz, knematz@purdue.edu
+	Team ID:        LC4-09
+	
+
+===============================================================================
+'''
 import numpy as np
 
+'''function to threshold the inputted png array'''
 def Threshold(image):
+    '''create empty array with same shape as inputted array'''
     result = np.empty(image.shape)
+    '''find average value of the array in order to figure out value to determine if an array value should be transformed to white or black'''
     average = np.average(image)
+    '''loop that iterates through each value in array to perform thresholding'''
     for i in range(len(image)):
         for j in range(len(image[0])):
             if image[i][j] >= average:
@@ -14,3 +33,12 @@ def Threshold(image):
     
     
     
+'''
+===============================================================================
+ACADEMIC INTEGRITY STATEMENT
+    I have not used source code obtained from any other unauthorized
+    source, either modified or unmodified. Neither have I provided
+    access to my code to another. The project I am submitting
+    is my own original work.
+===============================================================================
+'''
